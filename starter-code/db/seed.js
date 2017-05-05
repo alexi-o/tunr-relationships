3621462329da0d431bc9653d5e626daf291c9c8f
AlexiOHearn <alexiohearn@o.local>
2017-05-05 15:15:36 -0600

    finishes sprint 1

--- starter-code/db/seed.js
+++ starter-code/db/seed.js
@@ -1,5 +1,20 @@
 var DB = require("../models").models;
 
+var lucySongs = [
+	{
+		title: "O sole mio",
+		duration: "3:21",
+		date_of_release: "1990",
+		album_title: "Three Tenors in Concert"
+	},
+	{
+		title: "Nessun dorma",
+		duration: "3:21",
+		date_of_release: "1990",
+		album_title: "Three Tenors in Concert"
+	}
+];
+
 var artistCreate = function() {
 	return DB.Artist.create({
     name: 'Luciano Pavarotti',
@@ -7,6 +22,11 @@ var artistCreate = function() {
     nationality: 'Italiano',
     instrument: 'Voice',
     home_address: '1 Strada Roma'
+  }).then(function(artist){
+  	lucySongs.forEach(function(song){
+  		song.artistId = artist.id;
+  	});
+  	DB.Song.bulkCreate(lucySongs);
   });
 };
 
